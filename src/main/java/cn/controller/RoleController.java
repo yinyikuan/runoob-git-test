5433233506fd6b729f12a47001838f0972fa7f20
root <18613377447@163.com>
2019-11-14 21:05:03 +0800

    shan

--- src/main/java/cn/controller/RoleController.java
+++
@@ -1,9 +0,0 @@
-package cn.controller;
-
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class RoleController {
-
-
-}
--- src/main/webapp/js/deleteUser.js
+++
@@ -1,5 +0,0 @@
-function del(id) {
-    $.post("deleteUser", {"id": id}, function (data) {
-        $("#" + id).remove();
-    })
-}
